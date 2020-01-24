211769b2481430109a2f7fdf8fb8c69cd53ed9ab
Emilia <prettylotusflowerz@gmail.com>
2020-01-24 18:02:48 +0000

    first

---
+++ wp-content/themes/smtcwordpress/functions.php
@@ -0,0 +1,82 @@
+<?php
+// Add scripts and stylesheets
+function smtcwordpress_scripts() {
+	wp_enqueue_style( 'bootstrap', get_template_directory_uri() . '/css/bootstrap.min.css', array(), '4.0.0' );
+	wp_enqueue_style( 'blog', get_template_directory_uri() . '/css/blog.css' );
+	wp_enqueue_script( 'bootstrap', get_template_directory_uri() . '/js/bootstrap.min.js', array( 'jquery' ), '4.0.0', true );
+}
+
+add_action( 'wp_enqueue_scripts', 'smtcwordpress_scripts' );
+
+// Add Google Fonts
+function smtcwordpress_google_fonts() {
+				wp_register_style('OpenSans', 'http://fonts.googleapis.com/css?family=Open+Sans:400,600,700,800');
+				wp_enqueue_style( 'OpenSans');
+		}
+
+		add_action('wp_print_styles', 'smtcwordpress_google_fonts');
+		
+// WordPress Titles
+add_theme_support( 'title-tag' );
+
+// Custom settings
+function custom_settings_add_menu() {
+  add_menu_page( 'Custom Settings', 'Custom Settings', 'manage_options', 'custom-settings', 'custom_settings_page', null, 99 );
+}
+add_action( 'admin_menu', 'custom_settings_add_menu' );
+
+// Create Custom Global Settings
+function custom_settings_page() { ?>
+  <div class="wrap">
+    <h1>Custom Settings</h1>
+    <form method="post" action="options.php">
+       <?php
+           settings_fields( 'section' );
+           do_settings_sections( 'theme-options' );      
+           submit_button(); 
+       ?>          
+    </form>
+  </div>
+  <?php }
+  
+  // Twitter
+function setting_twitter() { ?>
+  <input type="text" name="twitter" id="twitter" value="<?php echo get_option( 'twitter' ); ?>" />
+  <?php }
+function setting_github() { ?>
+  <input type="text" name="github" id="github" value="<?php echo get_option('github'); ?>" />
+  <?php }
+function custom_settings_page_setup() {
+  add_settings_section( 'section', 'All Settings', null, 'theme-options' );
+  add_settings_field( 'twitter', 'Twitter URL', 'setting_twitter', 'theme-options', 'section' );
+  add_settings_field( 'github', 'Github URL', 'setting_github', 'theme-options', 'section' );
+  
+  register_setting('section', 'twitter');
+  register_setting('section', 'github');
+}
+add_action( 'admin_init', 'custom_settings_page_setup' );
+
+// Support Featured Images
+add_theme_support( 'post-thumbnails' );
+
+// Custom Post Type
+function create_custom_post() {
+	register_post_type( 'custom-post',
+			array(
+			'labels' => array(
+					'name' => __( 'Custom Post' ),
+					'singular_name' => __( 'Custom Post' ),
+			),
+			'public' => true,
+			'has_archive' => true,
+			'supports' => array(
+					'title',
+					'editor',
+					'thumbnail',
+				  'custom-fields'
+			)
+	));
+}
+add_action( 'init', 'create_custom_post' );
+
+ 
